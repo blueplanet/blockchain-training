3b07074dc5d1670cd940d835b1206699f10cc949
blueplanet <gyo@re-work.io>
2018-09-17 11:35:51 +0900

    トップ画面モック

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,159 @@
 <template>
   <div>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 class="text-primary my-0 mr-md-auto font-weight-bold">Nissin Cerity Site</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
-        <a href="#" class="p-2 text-dark">Login</a>
-      </nav>
-    </div>
-    <div class="main-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Cerity</h1>
-      <p class="lead">
-        Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.
-      </p>
-    </div>
-    <div class="container">
+    <nav class="navbar navbar-expand-md border-bottom shadow-sm">
+      <div class="container">
+        <a href="#" class="navbar-brand"><b>日新研修</b></a>
+        <button class="navbar-toggler" tye="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="navbar-item active">
+              <a href="#" class="nav-link">ホーム</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav text-light">
+            <li class="navbar-item active">
+              <a href="#" class="btn btn-outline-primary">ログイン</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container mt-4">
+      <h1 class="display-4 mb-4 text-center jp-font">修了証明書</h1>
+      <h4 class="jp-font text-center mb-4">
+        日新研修コースを修了した対象者に対して、修了証明書を提供するサイト
+      </h4>
 
+      <div class="row mb-4">
+        <div class="col-4">
+          <div class="card border-primary">
+            <div class="card-body">
+              <h5 class="card-title">スマートコントラクト開発</h5>
+              <p class="card-text">
+                <ul class="list-group light-group-flush">
+                  <li class="list-group-item">
+                    日程
+                    <div class="float-right">
+                      <span class="badge badge-primary">2018/06/30 ~ 2018/07/31</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    クラス
+                    <div class="float-right">
+                      <span class="badge badge-primary">基礎</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    言語
+                    <div class="float-right">
+                      <span class="badge badge-primary">中国語</span>
+                    </div>
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div class="card-footer">
+              <span>
+                <i class="fa fa-users"></i>
+                15 人
+              </span>
+              <span class='ml-4'>
+                <i class="fa fa-clock"></i>
+                12 時間
+              </span>
+              <a href="#" class="btn btn-primary btn-sm float-right">詳細</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">スマートコントラクト開発</h5>
+              <p class="card-text">
+                <ul class="list-group light-group-flush">
+                  <li class="list-group-item">
+                    日程
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">未定</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    クラス
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">上級</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    言語
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">中国語</span>
+                    </div>
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div class="card-footer">
+              <span>
+                <i class="fa fa-users"></i>
+                最大 20 人
+              </span>
+              <span class='ml-4'>
+                <i class="fa fa-clock"></i>
+                12 時間
+              </span>
+              <a href="#" class="btn btn-outline-primary disabled btn-sm float-right">未定</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">スマートコントラクト開発</h5>
+              <p class="card-text">
+                <ul class="list-group light-group-flush">
+                  <li class="list-group-item">
+                    日程
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">2018/09/29 ~ 2018/10/07</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    クラス
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">基礎</span>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    言語
+                    <div class="float-right">
+                      <span class="badge badge-primary badge-pill">日本語</span>
+                    </div>
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div class="card-footer">
+              <span>
+                <i class="fa fa-users"></i>
+                12 人
+              </span>
+              <span class='ml-4'>
+                <i class="fa fa-clock"></i>
+                12 時間
+              </span>
+              <a href="#" class="btn btn-outline-primary disabled btn-sm float-right">進行中</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
+    <footer class="mt-4 pt-4 text-center">
+      <div class="container border-top pt-3">
+        Copyright © 2018 · <a href="#"><b>NisSin 日新株式会社</b></a>
+      </div>
+    </footer>
   </div>
 </template>
 
